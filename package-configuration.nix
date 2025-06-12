c16a7951758b69b4acb0401b9cc938577fb4593a
Rinbadagin <klara78robinson@gmail.com>
2025-06-12 16:46:04 +1200

    Adjust librewolf cookie settings - Revision Thu 12 Jun 2025 16:46:04 NZST

--- package-configuration.nix
+++ package-configuration.nix
@@ -11,11 +11,8 @@
         "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
           "cookiebanners.service.mode" = 2; # Block cookie banners
           "privacy.donottrackheader.enabled" = true;
-        "privacy.fingerprintingProtection" = true;
-        "privacy.resistFingerprinting" = true;
+        "privacy.resistFingerprinting" = false;
         "privacy.trackingprotection.emailtracking.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
         "privacy.trackingprotection.socialtracking.enabled" = true;
         "privacy.clearOnShutdown.history" = false;
         "privacy.clearOnShutdown.cookies" = false;
