0c57cfff9e171997a877d6decb9c916c247b60d2
Rinbadagin <klara78robinson@gmail.com>
2025-06-19 21:01:06 +1200

    Fix wereshark ??

--- package-configuration.nix
+++ package-configuration.nix
@@ -3,28 +3,28 @@
   programs.steam.enable = true;
   programs.firefox = {
     enable = true;
-    package = pkgs.librewolf;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      Preferences = {
-        "webgl.disabled" = false;
-        "privacy.resistFingerprinting" = false;
-        "privacy.clearOnShutdown.history" = false;
-        "privacy.clearOnShutdown.cookies" = false;
-        "network.cookie.lifetimePolicy" = 0;
-      };
-      ExtensionSettings = {
-        "jid1-ZAdIEUB7XOzOJw@jetpack" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
+    #package = pkgs.librewolf;
+    #policies = {
+    #  DisableTelemetry = true;
+    #  DisableFirefoxStudies = true;
+    #  Preferences = {
+    #    "webgl.disabled" = false;
+    #    "privacy.resistFingerprinting" = false;
+    #    "privacy.clearOnShutdown.history" = false;
+    #    "privacy.clearOnShutdown.cookies" = false;
+    #    "network.cookie.lifetimePolicy" = 0;
+    #  };
+    #  ExtensionSettings = {
+    #    "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+    ##      install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+    #      installation_mode = "force_installed";
+    #    };
+    #    "uBlock0@raymondhill.net" = {
+    #      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+    #      installation_mode = "force_installed";
+    #    };
+    #  };
+    #};
   };
   programs.zsh.enable = true;
 
