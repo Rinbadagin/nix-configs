b10063f12fa910c13619eef1494ba429119bfdd0
Rinbadagin <klara78robinson@gmail.com>
2025-06-19 14:00:17 +1200

    Commits dont need fluff - Revision Thu 19 Jun 2025 14:00:17 NZST

--- scripts/editnix.zsh
+++ scripts/editnix.zsh
@@ -1,7 +1,8 @@
 #!/run/current-system/sw/bin/env zsh
 set -e
+sudo -- sh -c "vim /etc/nixos/"
 echo "Commit message?"
 msg=$(read -e)
-sudo -- sh -c "vim /etc/nixos/ && nixos-rebuild switch"
-(cd /etc/nixos/ && git add . && git commit -m "$msg - Revision $(date)" && git push)
+sudo -- sh -c "nixos-rebuild switch"
+(cd /etc/nixos/ && git add . && git commit -m "$msg" && git push)
 echo "Commited $msg"
