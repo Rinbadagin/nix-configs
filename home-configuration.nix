a6bc212b876f78c8d140bf684af4d978b81366dd
Rinbadagin <klara78robinson@gmail.com>
2025-06-20 14:42:18 +1200

    Add lua for telescope bindings

--- home-configuration.nix
+++ home-configuration.nix
@@ -15,6 +15,14 @@ in
       extraConfig = ''
         set autoindent expandtab tabstop=2 shiftwidth=2
         set number relativenumber
+
+        vim.cmd([[
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        ]])
         '';
       vimAlias = true;
       viAlias = true;
