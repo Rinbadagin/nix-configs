9fe6a0c5eb9f067fa7f231fa7cb744a9f7d04a91
Rinbadagin <klara78robinson@gmail.com>
2025-06-20 14:47:34 +1200

    Fix telescope stuff for real

--- home-configuration.nix
+++ home-configuration.nix
@@ -3,84 +3,81 @@ let
 home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 in
 {
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
-  home-manager.useGlobalPkgs = true;
-  home-manager.users.klara = {
-    /* The home.stateVersion option does not have a default and must be set */
-    programs.neovim = {
-      enable = true;
-      plugins = with pkgs.vimPlugins; [ vim-airline neo-tree-nvim lspsaga-nvim telescope-fzf-native-nvim ];
-      extraConfig = ''
-        set autoindent expandtab tabstop=2 shiftwidth=2
-        set number relativenumber
+	imports = [
+		(import "${home-manager}/nixos")
+	];
+	home-manager.useGlobalPkgs = true;
+	home-manager.users.klara = {
+		/* The home.stateVersion option does not have a default and must be set */
+		programs.neovim = {
+			enable = true;
+			plugins = with pkgs.vimPlugins; [ vim-airline neo-tree-nvim lspsaga-nvim telescope-fzf-native-nvim ];
+			extraConfig = ''
+				set autoindent expandtab tabstop=2 shiftwidth=2
+				set number relativenumber
 
-        vim.cmd([[
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-        ]])
-        '';
-      vimAlias = true;
-      viAlias = true;
-    };
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    programs.tmux = {
-      enable = true;
-      shell = "${pkgs.zsh}/bin/zsh";
-    };
-    programs.kitty = {
-      enable = true;
-      extraConfig = ''
-        background_opacity 0.9
-        '';
-    };
-    programs.git = {
-      enable = true;
-      userName = "Rinbadagin";
-      extraConfig = {
-        init.defaultBranch = "main";
-        safe.directory = "/etc/nixos";
-      };
-    };
-    programs.zsh = {
-      enable = true;
-      autosuggestion.enable = true;
-      enableCompletion = true;
-      history.size = 1000000;
-      initContent = lib.mkOrder 1500 ''
-        source /etc/nixos/scripts/runtmux.zsh
-        '';
-      shellAliases = {
-        editnix = "/etc/nixos/scripts/editnix.zsh";
-        tess = "f(){tesseract -l eng $@ | echo}f";
-      };
-      oh-my-zsh = {
-        enable = true;
-        theme = "agnoster";
-        plugins = [ "aliases" "git" "colorize" ];
-      };
-    };
-    home.stateVersion = "25.05";
-    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
-  };
-  home-manager.users.root = {
-    programs.neovim = {
-      enable = true;
-      plugins = with pkgs.vimPlugins; [ vim-airline neo-tree-nvim lspsaga-nvim ];
-      extraConfig = ''
-        set autoindent expandtab tabstop=2 shiftwidth=2
-        set number relativenumber
-        '';
-      vimAlias = true;
-      viAlias = true;
-    };
-    home.stateVersion = "25.05";
-  };
+				nnoremap <leader>ff <cmd>Telescope find_files<cr>
+				nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+				nnoremap <leader>fb <cmd>Telescope buffers<cr>
+				nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+				'';
+			vimAlias = true;
+			viAlias = true;
+		};
+		programs.fzf = {
+			enable = true;
+			enableZshIntegration = true;
+		};
+		programs.tmux = {
+			enable = true;
+			shell = "${pkgs.zsh}/bin/zsh";
+		};
+		programs.kitty = {
+			enable = true;
+			extraConfig = ''
+				background_opacity 0.9
+				'';
+		};
+		programs.git = {
+			enable = true;
+			userName = "Rinbadagin";
+			extraConfig = {
+				init.defaultBranch = "main";
+				safe.directory = "/etc/nixos";
+			};
+		};
+		programs.zsh = {
+			enable = true;
+			autosuggestion.enable = true;
+			enableCompletion = true;
+			history.size = 1000000;
+			initContent = lib.mkOrder 1500 ''
+				source /etc/nixos/scripts/runtmux.zsh
+				'';
+			shellAliases = {
+				editnix = "/etc/nixos/scripts/editnix.zsh";
+				tess = "f(){tesseract -l eng $@ | echo}f";
+			};
+			oh-my-zsh = {
+				enable = true;
+				theme = "agnoster";
+				plugins = [ "aliases" "git" "colorize" ];
+			};
+		};
+		home.stateVersion = "25.05";
+		/* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+	};
+	home-manager.users.root = {
+		programs.neovim = {
+			enable = true;
+			plugins = with pkgs.vimPlugins; [ vim-airline neo-tree-nvim lspsaga-nvim ];
+			extraConfig = ''
+				set autoindent expandtab tabstop=2 shiftwidth=2
+				set number relativenumber
+				'';
+			vimAlias = true;
+			viAlias = true;
+		};
+		home.stateVersion = "25.05";
+	};
 }
