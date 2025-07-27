5707590449f32070a978f61b7b970ad1657d8af5
Rinbadagin <klara78robinson@gmail.com>
2025-07-27 19:04:22 +1200

    Add wacom tablet support

--- configuration.nix
+++ configuration.nix
@@ -52,6 +52,8 @@
 # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  services.xserver.wacom.enable = true;
+
 # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
