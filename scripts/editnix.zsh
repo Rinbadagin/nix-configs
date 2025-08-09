36c12aeb4fdb5226125365f9e22868ab4e7dcb2b
Rinbadagin <klara78robinson@gmail.com>
2025-08-09 21:27:58 +1200

    New flags for editnix

--- scripts/editnix.zsh
+++ scripts/editnix.zsh
@@ -1,14 +1,25 @@
 #!/run/current-system/sw/bin/env zsh
 
+# this could do with some refactoring
+
 set -e
 
-if ! [ -z "$1"]; then
-  if [[ "$1" == "nocommit" ]]; then
+if ! [[ -z $1 ]];
+then
+  if [[ $1 == "nocommit" ]];
+  then
     # nocommit path goes here
     sudo -- sh -c "vim /etc/nixos/"
     sudo -- sh -c "nixos-rebuild switch"
     echo "Did the thing! have fun :3"
     exit 0
+  elif [[ $1 == "justcommit" ]];
+  then
+    echo "Commit message?"
+    msg=$(read -e)
+    sudo -- sh -c "nixos-rebuild switch"
+    (cd /etc/nixos/ && git add . && git commit -m "$msg" && git push)
+    echo "Commited $msg"
   else
     echo "optional nocommit flag, else fine"
     exit 1
