1d8f0b58a8f5af66bb0717a7abd140d381f215d4
Rinbadagin <klara78robinson@gmail.com>
2025-08-09 12:19:36 +1200

    Fixed background formats

--- home-configuration.nix
+++ home-configuration.nix
@@ -25,7 +25,7 @@ in
         }
 
         output eDP-1 {
-          background /etc/nixos/backgrounds/castle.webp fill
+          background /etc/nixos/backgrounds/castle.png fill
         }
       '';
     };
