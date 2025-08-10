d369001a271efe425710526c0de0b8381febd773
Rinbadagin <klara78robinson@gmail.com>
2025-08-10 15:22:48 +1200

    Adjust lua (my syntax was wrong :( )

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -2,22 +2,22 @@
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [ vim-airline neo-tree-nvim lspsaga-nvim telescope-fzf-native-nvim ];
-    extraConfig = ''
-      set autoindent expandtab tabstop=2 shiftwidth=2
-      set number relativenumber
+    extraLuaConfig = ''
+      vim.cmd([[
+          set autoindent expandtab tabstop=2 shiftwidth=2"
+          set number relativenumber
 
-      nnoremap <leader>ff <cmd>Telescope find_files<cr>
-      nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-      nnoremap <leader>fb <cmd>Telescope buffers<cr>
-      nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-
-      lua << EOF
-      require('neo-tree').setup({
-          use_libuv_file_watcher=true
-          follow_current_file.enabled=true
-          })
-    EOF
-      '';
+          nnoremap <leader>ff <cmd>Telescope find_files<cr>
+          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+          nnoremap <leader>fb <cmd>Telescope buffers<cr>
+          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+      ]])
+	neotreeConfig = {
+          use_libuv_file_watcher = true;
+	  follow_current_file = { enabled = true; };
+	}
+      require('neo-tree').setup(neotreeConfig)
+    '';
     vimAlias = true;
     viAlias = true;
   };
