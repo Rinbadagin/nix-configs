b1b6d945137abe5596342fcf88f3a19e1e77d3f7
Rinbadagin <klara78robinson@gmail.com>
2025-08-10 15:42:41 +1200

    Complete preview setup with snacks

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -15,6 +15,19 @@
       neotreeConfig = {
         use_libuv_file_watcher = true;
         follow_current_file = { enabled = true; };
+        window = {
+          mappings = {
+            ["P"] = {
+              "toggle_preview",
+              config = {
+                use_float = false,
+                -- use_image_nvim = true,
+                use_snacks_image = true,
+                title = 'Neo-tree Preview',
+              },
+            },
+          }
+        }
       }
     require('neo-tree').setup(neotreeConfig)
       '';
