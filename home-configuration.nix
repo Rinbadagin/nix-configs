275541cfad1ed57a3d1202dc316d258528eb8136
Rinbadagin <klara78robinson@gmail.com>
2025-11-01 09:10:55 +1300

    Add battery state to status

--- home-configuration.nix
+++ home-configuration.nix
@@ -34,7 +34,8 @@ in
       text = ''
         #!/run/current-system/sw/bin/env bash
         bat=$(sudo tlp-stat -b | grep "Charge" | sed -rn "s/.*([0-9]{2}).*/\1/p")
-        echo $bat% $(date +'%Y-%m-%d %X')
+        state=$(sudo tlp-stat -b | grep "/status" | sed -re "s/.*\s+([a-zA-Z]+)/\1/")
+        echo $bat% $state $(date +'%Y-%m-%d %X')
       '';
       executable = true;
     };
