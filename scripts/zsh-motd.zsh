815f3d4d430191f7e62add318d0b2c45d84c93ea
Rinbadagin <klara78robinson@gmail.com>
2025-11-01 09:49:39 +1300

    Add TODO.md printout in zsh motd

--- scripts/zsh-motd.zsh
+++ scripts/zsh-motd.zsh
@@ -3,3 +3,5 @@ echo "Handy shortcuts:"
 echo "^a (start of line), ^e (end of line) alt + b (back a word) alt + f (forward a word) !! (substitute previous command)"
 echo "(vim) w, b, e, ge (word navigation)"
 echo "remember to use ^r in zsh and zeal! it's so handy!"
+
+cat /etc/nixos/TODO.md
