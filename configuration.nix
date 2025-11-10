4e02d1868c12d454ab50876a335c9001c8514844
Rinbadagin <klara78robinson@gmail.com>
2025-11-10 19:09:22 +1300

    Add nix-command and flakes

--- configuration.nix
+++ configuration.nix
@@ -99,12 +99,14 @@
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.sudo.wheelNeedsPassword = false;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+# If you want to use JACK applications, uncomment this
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-# If you want to use JACK applications, uncomment this
+  
 #jack.enable = true;
 
 # use the example session manager (no others are packaged yet so this is enabled by default,
