9a3da39d27fe708640a18d8f791ee58507b06400
Rinbadagin <klara78robinson@gmail.com>
2025-11-14 22:09:34 +1300

    Add me to the docker group >:3

--- user-configuration.nix
+++ user-configuration.nix
@@ -3,7 +3,7 @@
   users.users.klara = {
     isNormalUser = true;
     description = "Klara";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
 # Managed by home-manager - do we need this?
