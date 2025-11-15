97847179aafa8977a689b1cde6793461fe24b4b0
Rinbadagin <klara78robinson@gmail.com>
2025-11-15 19:12:00 +1300

    Amend status bar battery indicator

--- scripts/status-bar.bash
+++ scripts/status-bar.bash
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/env bash
-bat=$(sudo tlp-stat -b | grep "Charge" | sed -rn "s/.*([0-9]{3}).*/\1/p")
+bat=$(sudo tlp-stat -b | grep "Charge" | sed -rn "s/.*([0-9\.]{4}).*/\1/p")
 state=$(sudo tlp-stat -b | grep "/status" | sed -re "s/.*\s+([a-zA-Z]+)/\1/")
 echo $bat% $state $(date +'%Y-%m-%d %X')
