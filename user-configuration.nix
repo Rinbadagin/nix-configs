401104b3d88cd8e4030c49919e8a770f9b678548
Rinbadagin <klara78robinson@gmail.com>
2025-12-29 11:55:52 +1300

    Add syncthing group for mee

--- user-configuration.nix
+++ user-configuration.nix
@@ -3,7 +3,7 @@
   users.users.klara = {
     isNormalUser = true;
     description = "Klara";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "syncthing" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
 # Managed by home-manager - do we need this?
