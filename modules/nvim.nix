ee9f5221dde383f70387dda481514e89e438f277
Rinbadagin <klara78robinson@gmail.com>
2026-01-26 16:43:54 +1300

    Made nixvim use 25.11

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -3,7 +3,7 @@ let
   nixvim = import (builtins.fetchGit {
     url = "https://github.com/nix-community/nixvim";
     # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
-    ref = "nixos-25.05";
+    ref = "nixos-25.11";
   });
 in
 {
