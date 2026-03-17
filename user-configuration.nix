913d911990f8203a0b1e36e95a8c35e8c460ff25
Rinbadagin <klara78robinson@gmail.com>
2026-03-17 22:46:43 +1300

    Give myself plugdev perms for udev rules

--- user-configuration.nix
+++ user-configuration.nix
@@ -3,7 +3,7 @@
   users.users.klara = {
     isNormalUser = true;
     description = "Klara";
-    extraGroups = [ "networkmanager" "wheel" "docker" "syncthing" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "syncthing" "plugdev" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
 # Managed by home-manager - do we need this?
